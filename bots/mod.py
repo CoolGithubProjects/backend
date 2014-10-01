e3db84340fd5fe30d5a4397b56b5ca24331b13e9
David Albertson <albertson.d@gmail.com>
2014-10-01 22:12:23 +0000

    Adding languages auto detection

--- bots/mod.py
+++ bots/mod.py
@@ -1,7 +1,35 @@
-import praw
 import time
+import json
+
+import praw
+import requests
+
 
 posts = []
+languages = ["python", "cpp", "c", "ruby", "d", "java", "javascript", "shell", "scala", "objective-c", \
+             "haskell", "emacs-lisp", "perl", "assembly", "csharp", "fortran", "go", "php", "common-lisp" \
+                                                                                            "erlang", "swift"]
+
+
+def getLanguage(link):
+    token = link.split("/")
+    user = token[-2]
+    repo = token[-1]
+    r = requests.get('https://api.github.com/repos/' + user + '/' + repo)
+    if (r.ok):
+        repoItem = json.loads(r.text or r.content)
+        language = repoItem['language'].replace(" ", "-").lower()
+
+        # quick fix for reddit css rules
+        if language == "c#":
+            return "csharp"
+        elif language == "c++":
+            return "cpp"
+
+        return language
+    else:
+        print "Unable to get language for url [" + link + "]"
+        return None
 
 
 def main():
@@ -16,26 +44,33 @@ def main():
             for post in submissions:
                 if post not in posts:
                     url = post.url
-                    if url.startswith("https://github.com") or url.startswith("http://github.com") or url.startswith("https://wwww.github.com") or url.startswith("https://github.com") or url.startswith("http://git.io") or url.startswith("https://git.io") or ".github.io" in url:
-                        #if post.link_flair_text == None:
-                            #print "No flair " + str(post.title)
-                            #l = getLanguage(url)
-
+                    if url.startswith("https://github.com") or url.startswith("http://github.com") or url.startswith(
+                            "https://wwww.github.com") or url.startswith("https://github.com") or url.startswith(
+                            "http://git.io") or url.startswith("https://git.io") or ".github.io" in url:
+                        if post.link_flair_text == None:
+                            lang = getLanguage(url)
+                            if lang in languages:
+                                post.set_flair(flair_text=lang.upper(), flair_css_class=lang)
+                                print "Setting language flair to [" + lang + "] for url [" + url + "]"
+                            else:
+                                post.set_flair(flair_text="OTHER", flair_css_class="other")
+                                print "Unsupported language [" + lang + "] for url [" + url + "]"
                         print "Valid post by : " + post.author.name + " " + url
+                        posts.append(post)
                     else:
                         author = post.author.name
                         if author == "Diastro" or author == "curlymaster" or author == "Chris911":
+                            posts.append(post)
                             continue
                         msg = '''The link you submitted does not point to a valid Github repository. If you think that this is a mistake, please contact the mods.'''
                         post.add_comment(msg)
                         post.remove()
                         print "Deleted : " + str(author) + " " + str(post.title) + " " + str(post.url)
-                    posts.append(post)
-        except Exception,e:
+        except Exception, e:
             print str(e)
             print "Error trying again in 120 seconds"
-            time.sleep(120)
-        time.sleep(60)
+            time.sleep(60)
+        time.sleep(15)
     return
 
 
